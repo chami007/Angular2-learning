c9654f37571a91c44d4d41645182a229e82dec48
Chamal Liyanage <cliyanage@hotmail.co.uk>
2016-10-07 07:14:39 +0100

    Added validation and stuff

--- src/app/forms/components/form-example1/form-example1.component.ts
+++ src/app/forms/components/form-example1/form-example1.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormGroup, FormControl, FormArray, FormBuilder, Validators} from '@angular/forms';
+import { AbstractControl, FormGroup,
+  FormControl, FormArray, FormBuilder,
+  Validators} from '@angular/forms';
 
 @Component({
   selector: 'fau-form-example1',
@@ -10,14 +12,34 @@ export class FormExample1Component {
 
   form: FormGroup;
   constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
+    this.form = fb.group({
       firstName: [''],
       lastName: [''],
-      username: [''],
-      password: [''],
-      confirm: [''],
+      account: fb.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required],
+        confirm: ['', Validators.required],
+      }, { validator: this.passwordMatcher }),
       newsletter: ['']
     });
+
+    this.form.patchValue({
+      firstName: ['Chamal'],
+      lastName: ['Liyanage'],
+      username: ['Chami007'],
+      password: ['pwd'],
+      confirm: ['pwd'],
+      newsletter: ['yes']
+    });
+  }
+
+  passwordMatcher(c: AbstractControl) {
+    return c.get('password').value === c.get('confirm').value
+      ? null : { 'nomatch': true };
+  }
+
+  onSubmit() {
+    console.log(this.form);
   }
 
 }
